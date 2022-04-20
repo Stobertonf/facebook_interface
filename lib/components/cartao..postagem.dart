878d5acfa1e83f111434ab4e347ec9b64d814e3f
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-19 22:42:31 -0300

    feat(post-list): Ccreating Post List

--- lib/components/cartao..postagem.dart
+++ lib/components/cartao..postagem.dart
@@ -1,13 +1,99 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook_interface/components/imagem_perfil.dart';
 import 'package:flutter/material.dart';
 
+import '../models/postagem.dart';
 
-class  extends StatelessWidget {
-  const ({ Key? key }) : super(key: key);
+class CartaoPostagem extends StatelessWidget {
+  final Postagem postagem;
+  const CartaoPostagem({Key? key, required this.postagem}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Column(
+        children: <Widget>[
+          //Cabeçalho
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CabecalhoPostagem(postagem: postagem),
+                //Cabeçalho
+                Text(
+                  postagem.descricao,
+                ),
+                //Descrição
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: CachedNetworkImage(
+              imageUrl: postagem.urlImagem,
+            ),
+          ),
+          //Imagem Postagem
+          //Area de estáticas
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CabecalhoPostagem extends StatelessWidget {
+  final Postagem postagem;
+
+  const CabecalhoPostagem({Key? key, required this.postagem}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ImagemPerfil(
+          urlImagem: postagem.usuario.urlImagem,
+          foiVisualizado: true,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                postagem.usuario.nome,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "${postagem.tempoAtras} -",
+                  ),
+                  Icon(
+                    Icons.public,
+                    size: 12,
+                    color: Colors.grey[600],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.more_horiz,
+          ),
+        ),
+      ],
     );
   }
 }
