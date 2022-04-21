e134972301fecfe1238c2feee94b8ae03632263c
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-21 15:51:33 -0300

    feat:(post-list): Creating statistics and post btn

--- lib/components/cartao..postagem.dart
+++ lib/components/cartao..postagem.dart
@@ -1,8 +1,9 @@
+import '../models/postagem.dart';
+import '../uteis/paleta_cores.dart';
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:facebook_interface/components/imagem_perfil.dart';
-import 'package:flutter/material.dart';
-
-import '../models/postagem.dart';
 
 class CartaoPostagem extends StatelessWidget {
   final Postagem postagem;
@@ -11,6 +12,9 @@ class CartaoPostagem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.symmetric(vertical: 8),
       child: Column(
         children: <Widget>[
           //Cabeçalho
@@ -36,10 +40,146 @@ class CartaoPostagem extends StatelessWidget {
           ),
           //Imagem Postagem
           //Area de estáticas
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: EstaticasPostagem(
+              postagem: postagem,
+            ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+// Estati
+class EstaticasPostagem extends StatelessWidget {
+  final Postagem postagem;
+
+  const EstaticasPostagem({
+    Key? key,
+    required this.postagem,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(4),
+              decoration: const BoxDecoration(
+                color: PaletaCores.azulFacebook,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.thumb_up,
+                size: 10,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              width: 4,
+            ),
+            //Expandindo até  o tamanho total disponível
+            Expanded(
+              child: Text(
+                "${postagem.curtidas}", //Curtidas
+                style: TextStyle(
+                  color: Colors.grey[700],
+                ),
+              ),
+            ),
+            Text(
+              "${postagem.comentarios}", //Comentários
+              style: TextStyle(
+                color: Colors.grey[700],
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              "${postagem.compartilhamentos}", //Compartilhamentos
+              style: TextStyle(
+                color: Colors.grey[700],
+              ),
+            ),
+          ],
+        ),
+        const Divider(
+          thickness: 1.2,
+        ),
+        Row(
+          children: [
+            BotaoPostagem(
+              icone: Icon(
+                LineIcons.thumbsUpAlt,
+                color: Colors.grey[700],
+              ),
+              texto: "Curtir",
+              onTap: () {},
+            ),
+            BotaoPostagem(
+              icone: Icon(
+                LineIcons.alternateCommentAlt,
+                color: Colors.grey[700],
+              ),
+              texto: "Comentar",
+              onTap: () {},
+            ),
+            BotaoPostagem(
+              icone: Icon(
+                LineIcons.share,
+                color: Colors.grey[700],
+              ),
+              texto: "Compartilhar",
+              onTap: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class BotaoPostagem extends StatelessWidget {
+  final Icon icone;
+  final String texto;
+  final VoidCallback onTap;
+
+  const BotaoPostagem({
+    Key? key,
+    required this.icone,
+    required this.texto,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //Interface
+    return Expanded(
+      child: Material(
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            child: Row(
+              children: [
+                icone,
+                const SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  texto,
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
