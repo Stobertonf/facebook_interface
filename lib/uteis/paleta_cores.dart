2c4ae58282d6f394c9042a81d4901f933e3b4c16
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-21 15:49:28 -0300

    Style:(scaffold-Corlor): Creating scaffold corlor

--- lib/uteis/paleta_cores.dart
+++ lib/uteis/paleta_cores.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class PaletaCores {
   static const Color azulFacebook = Color(0xFF1777F2);
+  static const Color scaffold = Color(0xFFF02F5);
   static const LinearGradient degradeEstoria = LinearGradient(
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter,
