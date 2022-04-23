2042ccb41676091b539ec771c735a1afce5562db
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-23 18:47:18 -0300

    feat(Top-Navigation-Bar): Creating btn img perfil

---
+++ lib/components/botao_imagem_perfil.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BotaoImagemPerfil extends StatelessWidget {
+  const BotaoImagemPerfil({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
