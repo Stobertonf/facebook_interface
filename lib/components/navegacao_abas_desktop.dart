7cead4cbc0707af22832b9af59003f6966029b3d
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-23 16:39:02 -0300

    feat(Responsive): Creating Desktop Tabs Navigation

--- lib/components/navegacao_abas_desktop.dart
+++ lib/components/navegacao_abas_desktop.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 
+import '../uteis/paleta_cores.dart';
+import 'navegacao_abas.dart';
+
 class NavegacaoAbasDesktop extends StatelessWidget {
-  const NavegacaoAbasDesktop({Key? key}) : super(key: key);
+  final List<IconData> icones;
+  final int indiceAbaSelecionada;
+  final Function(int) onTap;
+
+  const NavegacaoAbasDesktop({
+    Key? key,
+    required this.icones,
+    required this.indiceAbaSelecionada,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       height: 65,
-      decoration: BoxDecoration(color: Colors.white, boxShadow: [
-        BoxShadow(
-          color: Colors.black12,
-          offset: Offset(0, 2),
-          blurRadius: 4,
-        ),
-      ]),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 2),
+            blurRadius: 4,
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          const Expanded(
+            child: Text(
+              "Facebook",
+              style: TextStyle(
+                color: PaletaCores.azulFacebook,
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+                letterSpacing: -1.2,
+              ),
+            ),
+          ),
+          Expanded(
+            child: NavegacaoAbas(
+              icones: icones,
+              onTap: onTap,
+              indicadorEmbaixo: true,
+              indiceAbaSelecionada: indiceAbaSelecionada,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
