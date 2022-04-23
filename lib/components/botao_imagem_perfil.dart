b489ace4e6a69c232ae078fd457126fc54d9a85b
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-04-23 19:17:32 -0300

    feat(Top-Navigation-Bar): Creating Btn IMG Profile

--- lib/components/botao_imagem_perfil.dart
+++ lib/components/botao_imagem_perfil.dart
@@ -1,10 +1,38 @@
+import 'package:facebook_interface/components/imagem_perfil.dart';
+
+import '../models/usuario.dart';
 import 'package:flutter/material.dart';
 
 class BotaoImagemPerfil extends StatelessWidget {
-  const BotaoImagemPerfil({Key? key}) : super(key: key);
+  final Usuario usuario;
+  final VoidCallback onTap;
+
+  const BotaoImagemPerfil({
+    Key? key,
+    required this.usuario,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+        onTap: onTap,
+        child: Row(
+          children: [
+            ImagemPerfil(
+              urlImagem: usuario.urlImagem,
+              foiVisualizado: true,
+            ),
+            const SizedBox(
+              width: 4,
+            ),
+            Text(
+              usuario.nome,
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ));
   }
 }
